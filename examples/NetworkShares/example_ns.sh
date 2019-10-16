321f66ece35cb2a5d09345230880a1171f37707d
Seth Cagampang <seth.cagampang@osnexus.com>
2019-10-16 15:54:24 -0700

    example NS

---
+++ examples/NetworkShares/example_ns.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "not the right amount of args"
+    return 1
+fi
+
+echo "Begging Quatastor Network Share setup example"
+
+python3 shr_setup.py $1
+
+echo "Created Network Share: 'testShare'"
+
+if [ ! -d /mnt/testMount ]; then
+    sudo mkdir /mnt/testMount
+    echo "Created Directory: '/mnt/testMount'."
+else
+    echo "Dir: '/mnt/testMount' already exists."
+fi
+
+echo "Mounting share to '/mnt/testMount'." 
+
+sudo mount $1:/export/testShare/ /mnt/testMount/
+
+echo "Displaying active mount to 'testShare': "
+
+mount | grep -i "testShare"
